406f7e71b19c4f6feeb9788f3cb8e2026cb262cb
Nicholas <nicholas4747.wmnc@gmail.com>
2026-08-15 15:41:30 +0700

    Mambo

--- Test3.py
+++ Test3.py
@@ -1,5 +1,14 @@
 print("Finding the Area of a Triangle")
+
 x = input("What's the Height of the Triangle : ")
 y = input("What's the Base of the Triangle : ")
 
-print(f"The Answer is : {math. x*y/2}")
+x = float(x)
+y = float(y)
+z = x*y/2
+
+print(f"The Answer is : {z}")
+
+r = input("Run Again? [y/n]")
+if r != y
+break
