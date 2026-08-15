3a042c38046ddd802c395dbcd53dfa193bfa3365
Nicholas <nicholas4747.wmnc@gmail.com>
2026-08-15 15:32:55 +0700

    Mambo

---
+++ Test3.py
@@ -0,0 +1,5 @@
+print("Finding the Area of a Triangle")
+x = input("What's the Height of the Triangle : ")
+y = input("What's the Base of the Triangle : ")
+
+print(f"The Answer is : {math. x*y/2}")
---
+++ pythontest 2.py
@@ -0,0 +1,6 @@
+name = "Nicholas"
+age = "17"
+height = 1.75
+x = True
+
+print(type(age))
