336993ff3375cffd5fd2af6d5094fec60ffb9166
Nicholas <nicholas4747.wmnc@gmail.com>
2026-08-22 13:17:24 +0700

    Mamboo

--- Pythontest.py
+++
@@ -1,6 +0,0 @@
-for x in range (5):
-    print ('Mambo')
-    for y in range (1):
-        print('Hachimi')
-        for z in range (1):
-            print ("WEI!")
--- Test3.py
+++ Test3.py
@@ -1,15 +1,29 @@
-print("Finding the Area of a Triangle")
+n=1
+while (n != 'N'): 
 
-x = float(input("What's the Height of the Triangle : "))
-y = float(input("What's the Base of the Triangle : "))
+    print("Finding the Area of a Triangle")
 
-z = x*y/2
+    x = float(input("What's the Height of the Triangle : "))
+    y = float(input("What's the Base of the Triangle : "))
 
-print(f"The Area is : {z}")
+    z = x*y/2
 
-n = str(input('Continue? [y/N]'))
+    print(f"The Area is : {z}")
 
-if (n==y):
-    break
+    n = input('Continue? [y/N] : ')
+
+    if (n == 'N'):
+        print("Thank you for using this program")
+        break
+    elif (n == 'y'):
+        print("Here you go")
+        continue
+    else:
+        print("We'll take that as a no")
+        print('Thank you for using this program')
+        break
+
+
+
 
     
